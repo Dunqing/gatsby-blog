851483c7d179c5777744fc7e5ffc25c39f0ee1b0
dengqing <1247748612@qq.com>
2020-07-19 09:55:15 +0800

    🎨 改进结构更方便请求多个graphql接口

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,8 +6,23 @@
 
 // You can delete this file if you're not using it
 
-exports.createPages = async function ({ actions, graphql }) {
-  const { data } = await graphql(`
+const makeRequest = (graphql, request) => {
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(request).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+        return result
+      })
+    )
+  })
+}
+
+const getAllArticle = (graphql, actions) =>
+  makeRequest(
+    graphql,
+    ` 
     query {
       allStrapiArticle {
         edges {
@@ -18,13 +33,18 @@ exports.createPages = async function ({ actions, graphql }) {
         }
       }
     }
-  `)
-  data.allStrapiArticle.edges.forEach(edge => {
-    const strapiId = edge.node.strapiId
-    actions.createPage({
-      path: `article/${strapiId}`,
-      component: require.resolve(`./src/templates/article-template.tsx`),
-      context: { strapiId },
+  `
+  ).then(result => {
+    result.data.allStrapiArticle.edges.forEach(edge => {
+      const strapiId = edge.node.strapiId
+      actions.createPage({
+        path: `article/${strapiId}`,
+        component: require.resolve(`./src/templates/article-template.tsx`),
+        context: { strapiId },
+      })
     })
   })
+
+exports.createPages = async function ({ actions, graphql }) {
+  return Promise.all([getAllArticle(graphql, actions)])
 }
